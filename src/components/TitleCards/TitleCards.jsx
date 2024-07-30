0ccfc26d00040f5d8ab942646fb4c7d0d8b05be2
thearhansheth <arhan.sheth@gmail.com>
2024-07-30 20:04:31 +0530

    TitleCards Modified

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
   return (
     <div className='titleCards'>
-      
+      <h2>{title?title:"Popular on Notflix"}</h2>
+      <div className='cardList'>
+        {cards_data.map((card, index)=>{
+          return <div className='card' key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
