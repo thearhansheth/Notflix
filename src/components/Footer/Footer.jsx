06b8d0cb22a9267f6f312f5011263da9366fb018
thearhansheth <arhan.sheth@gmail.com>
2024-07-30 20:04:13 +0530

    Footer Elements Modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Footer.css'
+import ytubeIcon from "../../assets/youtube_icon.png"
+import XIcon from "../../assets/twitter_icon.png"
+import instaIcon from "../../assets/instagram_icon.png"
+import fBookIcon from "../../assets/facebook_icon.png"
 
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className='footerIcons'>
+        <img src={fBookIcon} alt="" />
+        <img src={instaIcon} alt="" />
+        <img src={XIcon} alt="" />
+        <img src={ytubeIcon} alt="" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyrightText'>© 2024 Notflix</p>
     </div>
   )
 }
