b0e82a8b7ab9855ab923eec7b434bce4dcece4f6
thearhansheth <arhan.sheth@gmail.com>
2024-07-30 11:28:25 +0530

    Created basic layout and navigation bar of home page

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Nav.css'
+import Logo from '../../assets/NotflixLogo.png'
+import search from '../../assets/search_icon.svg'
+import bell from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import drpdown from '../../assets/drpdown.svg'
+
+const Nav = () => {
+  return (
+    <div className='navBar'>
+      <div className="navbar-left">
+        <img src={Logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Upcoming</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search} alt='' className='icons'></img>
+        <p>Children</p>
+        <img src={bell} alt='' className='icons'></img>
+        <div className='navbar-profile'>
+          <img src={profile_img} alt='' className='profile'></img>
+          <img src={drpdown} alt=''></img>
+          <div className='dropdown'>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Nav from '../../components/Nav/Nav'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default Home
