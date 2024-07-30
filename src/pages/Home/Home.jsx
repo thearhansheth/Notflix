f854c70c02104f880ae54c2b66b284562866a2fa
thearhansheth <arhan.sheth@gmail.com>
2024-07-30 19:18:17 +0530

    Added buttons and hero banners for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Home.css'
 import Nav from '../../components/Nav/Nav'
+import heroBanner from '../../assets/hero_banner.jpg'
+import heroTitle from '../../assets/hero_title.png'
+import playIcon from '../../assets/play_icon.png'
+import infoIcon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Nav/>
+      <div className='hero'>
+        <img src={heroBanner} alt="" className='bannerImg'></img>
+        <div className='hero-caption'>
+          <img src={heroTitle} alt='' className='captionImg'></img>
+          <p>Discovering his ties to a secret ancient order, a young 
+            man living in modern Istanbul embarks on a quest to save the 
+            city from an immortal enemy.</p>
+            <div className='heroBtns'>
+              <button className='btn'><img src={playIcon} alt=''></img>Play</button>
+              <button  className='btn dark-btn'><img src={infoIcon} alt=''></img>More Info</button>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
