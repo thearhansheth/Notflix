45f1e3019310066836cf1c2345ec6df7ad5f01aa
thearhansheth <arhan.sheth@gmail.com>
2024-08-01 00:22:22 +0530

    Added login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { fbAuth } from './firebase'
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(fbAuth, async (user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/');
+      }else{
+        console.log("Logged Out");
+        navigate('/login');
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Routes>
