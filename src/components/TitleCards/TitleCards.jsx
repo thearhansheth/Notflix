47a2f86a8a2447119a2b1d550cd92b31cede07a2
thearhansheth <arhan.sheth@gmail.com>
2024-07-31 16:12:07 +0530

    Incorporated categories

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGM2MzNmZDhlM2YyZDg0YjU1MmYxNmFlNWVkZjM1MSIsIm5iZiI6MTcyMjQyMDk3My4xNzExMTYsInN1YiI6IjY2YWEwZGNhNDI4YWIwYzAzY2EzODk4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9jGWZIJNCB-MgrEKMLUncKcuXK8Plmzq1BB8eMNCG4Q'
+    }
+  };
+
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
   return (
     <div className='titleCards'>
       <h2>{title?title:"Popular on Notflix"}</h2>
       <div className='cardList'>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className='card' key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
