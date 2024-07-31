6bd45cdf21e15326f0e9114096a46f86955e29f9
thearhansheth <arhan.sheth@gmail.com>
2024-07-31 20:46:41 +0530

    Added player links

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 
 const TitleCards = ({title, category}) => {
 
@@ -24,10 +25,10 @@ const TitleCards = ({title, category}) => {
       <h2>{title?title:"Popular on Notflix"}</h2>
       <div className='cardList'>
         {apiData.map((card, index)=>{
-          return <div className='card' key={index}>
+          return <Link to={`/player/${card.id}`} className='card' key={index}>
             <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
             <p>{card.original_title}</p>
-          </div>
+          </Link>
         })}
       </div>
     </div>
